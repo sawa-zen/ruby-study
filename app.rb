9c4a072204b07ba107fa9277e512e5b5966aa2d7
sawada <sawadasuiren@gmail.com>
2015-08-21 21:15:52 +0900

    環境によって分岐できるように実装

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
+  configure :development do
+    puts 'develop mode!'
+  end
+
+  configure :production do
+    puts 'production mode!'
+  end
+
+  configure do
+    puts 'all configure!'
+  end
+
   get '/' do
     'hi'
   end
+
 end
